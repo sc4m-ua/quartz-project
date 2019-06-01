2c95c33b429f91262753a7da03ebc10f5f8ae8b0
sc4m-ua <47226524+sc4m-ua@users.noreply.github.com>
2019-06-02 01:07:36 +0300

    Update coins.js

--- custom_modules/coins.js
+++ custom_modules/coins.js
@@ -12,6 +12,7 @@ exports.run = async (message, database, cooldown, cmd, args, prefix)  => {
         if(!to) return message.reply("`правильное использование: /pay [пользователь] [количество].`");
         if(to.id == message.author.id) return message.reply("`вы не можете передать деньги самому себе.`");
         let count = args[2];
+        console.log(isNaN(count));
         if(isNaN(count)) return message.reply("`количество должно быть числом.`");
         parseInt(count);
         if(count < 1 || count > 250) return message.reply("вы не можете передать меньшего 1-го и больше 250-ти кварцов.");
