71aa58fff2878dce559866d4edccf71e0283492e
sc4m-ua <47226524+sc4m-ua@users.noreply.github.com>
2019-06-02 00:33:41 +0300

    Update coins.js

--- custom_modules/coins.js
+++ custom_modules/coins.js
@@ -4,8 +4,31 @@ exports.run = async (message, database, cooldown, cmd, args, prefix)  => {
     let db_channel = database.channels.find(c => c.name == message.author.id);
     if(!db_channel) return;
     if(cmd == `${prefix}bal`){
-        message.delete();
         message.reply(`\`ваш баланс составляет ${db_channel.topic.split(";")[2]} кварцов.\``);
+    }
+    if(cmd == `${prefix}pay`){
+        message.delete();
+        let to = message.mentions.members.first();
+        if(!to) return message.reply("`правильное использование: /pay [пользователь] [количество].`");
+        if(to.id == message.author.id) return message.reply("`вы не можете передать деньги самому себе.`");
+        let count = args[2];
+        if(isNan(count)) return message.reply("`количество должно быть числом.`");
+        let count = parseInt(count);
+        if(count < 1 || count > 250) return message.reply("вы не можете передать меньшего 1-го и больше 250-ти кварцов.");
+        let db_first = database.channels.find(c => c.name == message.member.id);
+        let db_second = database.channels.find(c => c.name == to.id);
+        if(!db_first) return message.reply("`ваш баланс составляет 0 кварцов.`");
+        if(!db_second){
+            database.createChannel(to.id, {type: "text"}).then(channel => {
+                channel.setParent("583967184208920576");
+                channel.setTopic(`${to.id};0;0`);
+                db_second = channel;
+            });
+        }
+        if(count > parseInt(db_second.topic.split(";")[2])) return message.reply("`у вас нет столько кварцов.");
+        db_first.setTopic(`${db_first.topic.split(";")[0]};${parseInt(db_first.topic.split(";")[1]) - count};${db_first.topic.split(";")[2]}`);
+        db_second.setTopic(`${db_second.topic.split(";")[0]};${parseInt(db_second.topic.split(";")[1]) + count};${db_second.topic.split(";")[2]}`);
+        message.channel.send(`${to}, на ваш счет зачислено ${count} кварцов. Источник: ${message.member}`);
     }
     if(cooldown.has(message.author.id)) return;
     db_channel.setTopic(`${message.author.id};${db_channel.topic.split(";")[1]};${parseInt(db_channel.topic.split(";")[2]) + 1}`);
