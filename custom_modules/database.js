0f95068123d7e884c6998f405cb6909450b80960
sc4m-ua <47226524+sc4m-ua@users.noreply.github.com>
2019-06-03 03:49:56 +0300

    Update database.js

--- custom_modules/database.js
+++ custom_modules/database.js
@@ -4,9 +4,9 @@ exports.run = async (message, database, cmd, args, prefix) => {
     if(message.guild.id != database.id) return;
     if(cmd == `${prefix}find`){
         let id = args[0];
-        if(!id) return message.reply("`вы не указали название id искаемого объекта.`");
+        if(!id) return message.reply("`вы не указали id искаемого объекта.`");
         let channel = database.channels.find(c => c.name == args[0]);
         if(!channel) return message.reply("`канал не найден.`");
-        message.channel.send(`**По вашему запросу найден следующий канал: <#${channel.id}>.**`);
+        message.channel.send(`**По вашему запросу найден следующий объект: <#${channel.id}>.**`);
     }
 }
