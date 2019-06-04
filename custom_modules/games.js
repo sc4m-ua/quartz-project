8945d1f1e1d0e77c92382fecf02ffc5bb3cbacfb
sc4m-ua <47226524+sc4m-ua@users.noreply.github.com>
2019-06-04 04:32:38 +0300

    Update games.js

--- custom_modules/games.js
+++ custom_modules/games.js
@@ -12,7 +12,7 @@ exports.run = async (message, database, cmd, args, prefix) => {
     if(count < 1 || count > 1000) return message.reply("`ставка должна быть в диапазоне от 1 до 1000.`");
     let msg = await message.channel.send(`${player}, \`${message.member.displayName} предлагает вам сыграть в кости. Ставка: ${count}. Нажмите на галочку, чтобы принять предложени.\``);
     msg.react("✔");
-    let filter = (reaction, user) => reaction.emoji.name == "✔" && user.id = player.id;
+    let filter = (reaction, user) => reaction.emoji.name === '✔' && user.id === 'someID'
     let collector = msg.createReactionCollector(filter, {time: 30000});
     collector.on('collect', reaction => {
       //some funcs here
