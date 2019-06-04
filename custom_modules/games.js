7172730a29c7e28b2016b0820f74444343a5e3f1
sc4m-ua <47226524+sc4m-ua@users.noreply.github.com>
2019-06-04 03:35:21 +0300

    Update games.js

--- custom_modules/games.js
+++ custom_modules/games.js
@@ -2,13 +2,19 @@ const Discord = require('discord.js');
 
 exports.run = async (message, database, cmd, args, prefix) => {
   if(cmd == `${prefix}dice`){
-    let user = message.mentions.members.first();
+    let player = message.mentions.members.first();
     if(!user) return message.reply("`правильное использование команды: !pay [пользователь] [ставка].`");
-    let count = args[0];
+    let count = args[1];
     if(!count) return message.reply("`правильное использование команды: !pay [пользователь] [ставка].`");
     if(isNaN(count)) return message.reply("`ставка должна иметь числовое значение.`");
     count = parseInt(count);
     if(count < 1 || count > 1000) return message.reply("`ставка должна быть в диапазоне от 1 до 1000.`");
-    message.channel.send(`${user}, \`${message.member.displayName} предлагает вам сыграть в кости. Ставка: ${count}. Нажмите на галочку, чтобы принять предложени.\``);
+    let msg await = message.channel.send(`${user}, \`${message.member.displayName} предлагает вам сыграть в кости. Ставка: ${count}. Нажмите на галочку, чтобы принять предложени.\``);
+    msg.react("✔");
+    let filter = (reaction, user) => reaction.emoji.name == "✔" && user.id = player.id;
+    let collector = msg.createReactionCollector(filter, {time: 30000});
+    collector.on('collect', reaction => {
+      //some funcs here
+    });
   }
 }
