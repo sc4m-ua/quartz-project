0758b54fe727c824a091ac8b823e6b225dfb3a90
sc4m-ua <47226524+sc4m-ua@users.noreply.github.com>
2019-06-10 19:03:33 +0300

    Update index.js

--- index.js
+++ index.js
@@ -245,15 +245,18 @@ async function autoDelete(){
         main.channels.forEach(async channel => {
             if(!channel.name.startsWith("appeal-")) return;
             let createdAt = channel.createdAt.valueOf();
+            console.log("Имя норм")
             let db_channel = await database.channels.find(c => c.name == channel.id);
             if(!db_channel) return main.channels.find(c => c.name == "bot-logs").send(`\`[APPEAL] Обращение №${channel.name.split("-")[1]} было удалено, так как не было найдено в базе данных. Сообщите о данной ошибке техническим администраторам.\``);
             let msgs = await db_channel.fetchMessages({limit: 10});
             if(msgs.size > 1) return console.log("Канал содержит больше одного сообщения.")
             msgs.forEach(async temp => {
+                console.log("Статус тоже норм")
                 if(temp.content.split("\n")[1] != "2") return;
                 if(date - createdAt >= 172800000){
                     channel.delete();
                     db_channel.delete();
+                    console.log("Последний шаг")
                     main.channels.find(c => c.name == "bot-logs").send(`\`[APPEAL] Обращение №${channel.name.split("-")[1]} было удалено. Причина: 48 часов в статусе "Закрыто".\``);
                 }
             });
