f43ff20679c677d6f16bb58acfae50a644133532
sc4m-ua <47226524+sc4m-ua@users.noreply.github.com>
2019-06-17 00:33:40 +0300

    Update games.js

--- custom_modules/games.js
+++ custom_modules/games.js
@@ -1,7 +1,11 @@
 const Discord = require('discord.js');
 
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
 exports.run = async (message, database, cmd, args, prefix) => {
-  if(cmd == `${prefix}dice`){
+  /*if(cmd == `${prefix}dice`){
     message.delete();
     let player = message.mentions.members.first();
     if(!player) return message.reply("`правильное использование команды: !dice [пользователь] [ставка].`");
@@ -20,5 +24,18 @@ exports.run = async (message, database, cmd, args, prefix) => {
     collector.on('end', collected => {
       if(!collected) return message.reply("`пользователь отказался от игры.`")
     })
+  }*/
+  if(cmd == `${prefix}wheel`){
+    if(!args[0]) return message.reply("`правильное использование команды: !wheel [ставка]`");
+    if(isNaN(args[0])) return message.reply("`ставка должна быть числом.`");
+    if(parseInt(args[0]) < 1 || parseInt(args[0]) > 1000) return message.reply("`ставка должна быть в диапазоне от 1 до 1000.`");
+    let random = getRandomInt(100);
+    let multy;
+    if(random <= 20) multy = 1;
+    if(random > 20 && random <= 50) multy = 0;
+    if(random > 50 && random <= 55) multy = 2;
+    if(random > 60 && random <= 70) multy = 1.5;
+    if(random > 70 && random <= 85) multy = 1.1;
+    let text = "";
   }
 }
