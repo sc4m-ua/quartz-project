f3dd858c4b92d81a57d9e2df7a4a0aa3f292805c
sc4m-ua <47226524+sc4m-ua@users.noreply.github.com>
2019-06-20 15:21:33 +0300

    Update shop_start.js

--- custom_modules/shop_start.js
+++ custom_modules/shop_start.js
@@ -1,11 +1,14 @@
-const Discord = require('discord.js')
-
+const Discord = require('discord.js');
 exports.run = async (message, cmd, args, prefix, database) => {
   if(cmd == `${prefix}shop_start`){
       if(!message.author.id != "347827337137750016") return;
       message.delete();
       let channel = await message.guild.channels.find(c => c.name == "shop");
       if(!channel) return console.log("Канал магазина не найден.")
-      
+      let embed = new Discord.RichEmbed()
+        .setAuthor("Quartz Shop")
+        .addField("Список товаров:", `**Товары не найдены.**`)
+        .setDescription("В этом канале вы можете купить уникальные товары за валюту дискор-сервера - кварцы.")
+      channel.send(embed)
   }
 } 
