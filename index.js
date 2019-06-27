81ae763f4f4e071e50da5f24367feca3a22d95c0
sc4m-ua <47226524+sc4m-ua@users.noreply.github.com>
2019-06-28 01:30:55 +0300

    Update index.js

--- index.js
+++ index.js
@@ -252,7 +252,23 @@ client.on('roleUpdate', async role => {
     });
 });
 
-client.on('raw', console.log);
+client.on('raw', async packet => {
+    if (!['MESSAGE_REACTION_ADD', 'MESSAGE_REACTION_REMOVE'].includes(packet.t)) return;
+    const channel = client.channels.get(packet.d.channel_id);
+    if (channel.messages.has(packet.d.message_id)) return;
+    channel.fetchMessage(packet.d.message_id).then(message => {
+        const emoji = packet.d.emoji.id ? `${packet.d.emoji.name}:${packet.d.emoji.id}` : packet.d.emoji.name;
+        const reaction = message.reactions.get(emoji);
+        if (reaction) reaction.users.set(packet.d.user_id, client.users.get(packet.d.user_id));
+        if (packet.t === 'MESSAGE_REACTION_ADD') {
+            client.emit('messageReactionAdd', reaction, client.users.get(packet.d.user_id));
+        }
+    });
+});
+
+client.on('messageReactionAdd', async (reaction, user) => {
+    console.log("done!")
+});
 
 client.login(process.env.BOT_TOKEN);
 
