d5c3ac3825b362570c4af9e783366231b919634f
sc4m-ua <47226524+sc4m-ua@users.noreply.github.com>
2019-06-28 01:49:33 +0300

    Update index.js

--- index.js
+++ index.js
@@ -255,19 +255,23 @@ client.on('roleUpdate', async role => {
 client.on('raw', async packet => {
     if (!['MESSAGE_REACTION_ADD', 'MESSAGE_REACTION_REMOVE'].includes(packet.t)) return;
     const channel = client.channels.get(packet.d.channel_id);
+    if(channel.name != "📞выдача-ролей") return;
     if (channel.messages.has(packet.d.message_id)) return;
     channel.fetchMessage(packet.d.message_id).then(message => {
         const emoji = packet.d.emoji.id ? `${packet.d.emoji.name}:${packet.d.emoji.id}` : packet.d.emoji.name;
         const reaction = message.reactions.get(emoji);
         if (reaction) reaction.users.set(packet.d.user_id, client.users.get(packet.d.user_id));
         if (packet.t === 'MESSAGE_REACTION_ADD') {
+            if(channel.name != "📞выдача-ролей") return;
+            if(!message.author.bot) return;
             client.emit('messageReactionAdd', reaction, client.users.get(packet.d.user_id));
         }
     });
 });
 
 client.on('messageReactionAdd', async (reaction, user) => {
-    console.log("done!")
+    if(reaction.emoji.name == "✅") console.log("галочка");
+    if(reaction.emoji.name == "❌") console.log("крестик");
 });
 
 client.login(process.env.BOT_TOKEN);
